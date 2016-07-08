3ecb91b397f074acfaad273fe30648f62b2410eb
phillyDesignr <jeremy.zevin@gmail.com>
2016-07-08 14:50:39 -0400

    Added color method

--- build/random.js
+++ build/random.js
@@ -5,33 +5,57 @@
  * Released under the MIT license
  * http://opensource.org/licenses/MIT
  *
- * Date: 1/11/16
+ * Date: 7/8/16
  *
  */
 "use strict";
 var Random = (function () {
     function Random() {
-        this.nativeRandom = Math.random;
+        this.native = Math.random;
     }
     //random integer beyween min and max
     Random.prototype.int = function (min, max) {
         if (min === void 0) { min = 0; }
         if (max === void 0) { max = 1; }
-        return Math.floor(this.nativeRandom() * (max - min + 1) + min);
+        return Math.floor(this.native() * (max - min + 1) + min);
     };
     //random real number between min and max
     Random.prototype.real = function (min, max) {
         if (min === void 0) { min = 0.0; }
         if (max === void 0) { max = 1.0; }
-        return this.nativeRandom() * (max - min) + min;
+        return this.native() * (max - min) + min;
     };
     //random pick from an array
     Random.prototype.pick = function (array) {
         return array[this.int(0, array.length - 1)];
     };
+    //boolean result based on result of expression
     Random.prototype.chance = function (percent) {
         if (percent === void 0) { percent = 10; }
-        return percent / 100 > this.nativeRandom() ? true : false;
+        return percent / 100 > this.native() ? true : false;
+    };
+    //random color rgb,rgba,hsl,hsla,hex
+    Random.prototype.color = function (operation) {
+        if (operation === void 0) { operation = "rgb"; }
+        if (operation === 'hex') {
+            var out = "#";
+            for (var i = 0; i <= 5; i++) {
+                out += this.pick([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]);
+            }
+            return out;
+        }
+        else if (operation === 'rgba') {
+            return "rgba(" + this.int(0, 255) + "," + this.int(0, 255) + "," + this.int(0, 255) + "," + this.int(0, 255) + ")";
+        }
+        else if (operation === 'hsl') {
+            return "hsl(" + this.int(0, 360) + "," + this.int(0, 100) + "%," + this.int(0, 100) + "%)";
+        }
+        else if (operation === 'hsla') {
+            return "hsla(" + this.int(0, 360) + "," + this.int(0, 100) + "%," + this.int(0, 100) + "%," + this.int(0, 100) + "%)";
+        }
+        else {
+            return "rgb(" + this.int(0, 255) + "," + this.int(0, 255) + "," + this.int(0, 255) + ")";
+        }
     };
     return Random;
 }());
